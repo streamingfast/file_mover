bafd64ee3452b94bc5c9efcfb7cc6098e3aab5a5
billettc <charles@cbillette.com>
2023-04-28 07:48:10 -0400

    max folder sizes

--- cmd/file_mover/main.go
+++ cmd/file_mover/main.go
@@ -4,9 +4,11 @@ import "file_mover"
 
 func main() {
 	folders := map[file_mover.SourceFolder]*file_mover.DestinationFolder{
-		file_mover.SourceFolder("/tmp/pic"): file_mover.NewDestinationFolder("/mnt/data/pic", 1000000000),
-		file_mover.SourceFolder("/tmp/4k"):  file_mover.NewDestinationFolder("/mnt/data/4k", 1000000000),
+		file_mover.SourceFolder("/tmp/pic"): file_mover.NewDestinationFolder("/mnt/data/pic", 21474836480),
+		file_mover.SourceFolder("/tmp/4k"):  file_mover.NewDestinationFolder("/mnt/data/4k", 21474836480),
 	}
+
+	//todo: create source and destination folders if they don't exist
 
 	mover := file_mover.NewMover(folders)
 	err := mover.Move() //blocking call
